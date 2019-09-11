da1bebc57e13aa91df365a0574c12022f38d88e0
madhanrajR <madhanit31@gmail.com>
2019-09-11 14:36:31 +0530

    config

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require("express");
-var cors = require("cors");
+var bodyparser = require("body-parser");
 var jwt = require("jsonwebtoken");
-var bodyParser= require("body-parser");
+var config = require("./config")
 var mongoose = require("mongoose");
+var cors = require("cors");
 var app = express();
+
+protectedroutes = express.Router();
+app.set('setsecret',config.secret)
+app.use('/api',protectedroutes);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
+
     var url = "mongodb+srv://sriram:sriram@cluster0-htdg9.mongodb.net/"
+    // var url = "mongodb://127.0.0.1:27017/"
     var DBname = "myemployee"
     
     mongoose.connection.on('connected', function() {
@@ -38,40 +45,54 @@ const PersonModel = mongoose.model("person", {
     phonenumber: String
 });
 
-app.post('/reg',(req,res)=>{
-    console.log(req.body)
-    var reg = new PersonModel({
-  "firstname":req.body.firstname,
-  "lastname":req.body.lastname,
-  "username":req.body.username,
-  "password":req.body.password,
-  "phonenumber":req.body.phonenumber
-    })
-  reg.save().then((result)=>{
-      if(result)
- res.json({"status":"success"})
- res.end();
-  }).catch(err =>{
-      res.send(err);
-      res.end();
-  })
-
+protectedroutes.use((req,res,next)=>{
+    
+    var token = req.headers['access-token']
+    console.log(token,"hai")
+    if(token){
+        jwt.verify(token,app.get('setsecret'),(err,data)=>{
+            if(err){
+                return res.json({"msg":"invalid token"})
+            } else {
+                req.decoded = data;
+                next();
+            }
+        })
+    } else{
+        return res.send({messge:'no token provided'})
+    }
+    // res.end();
 })
+protectedroutes.use(bodyparser.json());
+protectedroutes.use(bodyparser.urlencoded({extended:true}));
 
-app.post('/login',(req,res)=>{
+app.post("/login",(req,res)=>{
+
     var username  = req.body.username;
     var password =  req.body.password;
-    PersonModel.find({username,password}).then(result =>{
+    PersonModel.find({username,password}).then(result =>{               
         if(result){
             // console.log(result[0]['password'])
             if(result[0]['username'] == username && result[0]['password'] == password){
-                console.log("correnct")
-                var token = jwt.sign(username,password);
-                var data =  {
-                    "Data":result,
-                    "token":token
-                }
-                res.json(data)
+                console.log("correnct")      
+             
+                jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data:req.body.name },app.get('setsecret'),(err,tok)=>{
+                    // jwt.sign(req.body.name,app.get('setsecret'),{expiresIn: Math.floor(Date.now() / 1000) + (60 * 1) },(e,d)=>{
+                        if(err){
+                            console.log(err)
+                            res.end(err)
+                            
+                        }   
+                        res.json({"Date":result,"token":tok})
+                        res.end();
+                    })
+
+                    
+               
+                // res.json("unable to create token")
+                // res.end();
+            }else{
+                res.json({"status":"false","msg":"incorrect username or password"})
                 res.end();
             }           
         }           
@@ -80,10 +101,43 @@ app.post('/login',(req,res)=>{
         res.send(err)
         res.end();
     })
-   })
+})
 
-   app.delete('/deluser',(req,res)=>{
-    var username  = req.body.username;
+app.post('/reg',(req,res)=>{
+    req.p
+    console.log(req.body)
+    var reg = new PersonModel({
+        "firstname":req.body.firstname,
+        "lastname":req.body.lastname,
+        "username":req.body.username,
+        "password":req.body.password,
+        "phonenumber":req.body.phonenumber
+    })
+    reg.save().then((result)=>{
+        if(result)
+    res.json({"status":"success"})
+    res.end();
+    }).catch(err =>{
+        res.send(err);
+        res.end();
+    })
+})
+
+protectedroutes.put('/updateuser',(req,res)=>{    
+    var username  = req.body.username;        
+    PersonModel.findOneAndUpdate({username},{$set:req.body}).then(result =>{
+        if(result){
+            res.json({"status":true,"msg":"success","data":result})            
+        }
+    }).catch(err =>{
+        res.send(err);
+        res.end();
+    })    
+})
+
+protectedroutes.delete('/deluser/:username',(req,res)=>{
+    
+    var username  = req.params.username;
     console.log(username);
     PersonModel.deleteOne({username}).then(result =>{
         if(result){
@@ -94,26 +148,38 @@ app.post('/login',(req,res)=>{
         res.send(err);
         res.end();
     })
-   })
-
-app.get('/allusers',(req,res)=>{
- PersonModel.find({}).then(result =>{
-     res.json(result);
-     res.end();
- }).catch(err =>{
-     res.send(err);
-     res.end();
- })
 })
-app.get('/test',(req,res)=>{
-    res.write("Tested Connected");
+
+
+
+protectedroutes.get('/allusers',(req,res)=>{    
+
+    PersonModel.find({},(err,data)=>{
+        if(err){
+            console.log(err,"Error")
+            return res.end();
+        }
+        if(data){
+            console.log(data)
+            return res.json(data);            
+        }
+    })   
+})
+
+protectedroutes.get("/",(req,res)=>{
+    res.write("status connected");
     res.end();
 })
-app.get('/',(req,res)=>{
-    res.end('Connected');
+
+protectedroutes.post("/post/:param",(req,res) =>{
+    console.log(req.params.param,"asdf")  
+    res.json("payload");
 })
 
+protectedroutes.get("/payload/",(req,res)=>{
+    res.json("hai")
+})
 
-//var port = process.env.PORT || 3000;
-//console.log("server is listineg",port)
-app.listen(8080);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log(port);
